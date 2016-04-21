3786cb290fb90a131459e2f165f77f89127c767a
dmolin <davide.molin@gmail.com>
2016-04-21 18:16:56 +0100

    rephrasing values

--- client/components/MyValues.js
+++ client/components/MyValues.js
@@ -8,35 +8,32 @@ class MyValues extends React.Component {
       <div className="container">
         <header className="section-header">
           <h1 className="section-title">My values</h1>
+          <p>Here a brief selection of some of the things that make me happy and I look for and some of those that make this developer a sad puppy! :( </p>
         </header>
         <div className="section-content pure-grid">
           <section className="my-values-section my-values-section--yes pure-u-1 pure-u-md-1-2">
-            <h2>My "yes"</h2>
+            <h2>Happy days!</h2>
 
             <ul>
-              <li className="my-values-item">I Love working <strong>full time</strong> on Javascript/Html/CSS</li>
-              <li className="my-values-item">I live and work in Central London</li>
-              <li className="my-values-item">I like balancing work and private life</li>
-              <li className="my-values-item"><strong>Yes</strong> quick stand up</li>
-              <li className="my-values-item"><strong>Yes</strong> SCRUM</li>
-              <li className="my-values-item"><strong>Yes</strong> let's do-it approach</li>
-              <li className="my-values-item"><strong>Yes</strong> problem solving</li>
-              <li className="my-values-item"><strong>Yes</strong> flat/horizontal hierarchies</li>
-              <li className="my-values-item"><strong>Yes</strong> chats/emails</li>
-              <li className="my-values-item"><strong>Yes</strong> Unit Testing</li>
-              <li className="my-values-item"><strong>Yes Apple Macs & Linux</strong></li>
-              <li className="my-values-item"><strong>Yes</strong> remote working and flexi time(a <strong>BIG</strong> YES)</li>
+              <li className="my-values-item">Working in Central London</li>
+              <li className="my-values-item">Having the time to balance work and private life</li>
+              <li className="my-values-item">SCRUM</li>
+              <li className="my-values-item">Let's do-it approach</li>
+              <li className="my-values-item">Problem solving attitude</li>
+              <li className="my-values-item">Flat/horizontal hierarchies</li>
+              <li className="my-values-item">Unit Testing</li>
+              <li className="my-values-item">Apple Macs & Linux</li>
+              <li className="my-values-item">Remote working and flexi time(a <strong>VERY HAPPY</strong> puppy!)</li>
             </ul>
           </section>
           <section className="my-values-section my-values-section--no pure-u-1 pure-u-md-1-2">
-            <h2>My "no"</h2>
+            <h2>Sad face</h2>
 
             <ul>
-              <li className="my-values-item"><strong>No</strong> Traveling</li>
-              <li className="my-values-item">I'm not interested in permanent opportunities. </li>
-              <li className="my-values-item">I try to avoid more than 9 hours/day at the office (but I can continue working from home if necessary)</li>
-              <li className="my-values-item">I'm not a fan of the practice of <a href="https://en.wikipedia.org/wiki/Pair_programming" target="_blank">"Agile Alliance XP pair-programming"</a> (this is not the same as 'working with a colleague".. I love that!)</li>
-              <li className="my-values-item"><strong>Preferably No MS Windows</strong></li>
+              <li className="my-values-item">Traveling</li>
+              <li className="my-values-item">Working more than 9 hours/day at the office (but I can continue working from home, no probl. with that)</li>
+              <li className="my-values-item">The practice of <a className="link" href="https://en.wikipedia.org/wiki/Pair_programming" target="_blank">"Agile Alliance XP pair-programming"</a> (this is not the same as 'working with a colleague".. I love that!)</li>
+              <li className="my-values-item">The use of MS Windows</li>
             </ul>
           </section>
         </div>
