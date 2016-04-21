dc9f5a349964d20481e85d2bbddd3f611f9c3cd7
dmolin <davide.molin@gmail.com>
2016-04-21 18:23:22 +0100

    rephrasing

--- client/components/MyValues.js
+++ client/components/MyValues.js
@@ -8,7 +8,7 @@ class MyValues extends React.Component {
       <div className="container">
         <header className="section-header">
           <h1 className="section-title">My values</h1>
-          <p>Here a brief selection of some of the things that make me happy and I look for and some of those that make this developer a sad puppy! :( </p>
+          <p>Here is a brief selection of some of the things that make me happy and I look for and some of those that make this developer a sad puppy! :( </p>
         </header>
         <div className="section-content pure-grid">
           <section className="my-values-section my-values-section--yes pure-u-1 pure-u-md-1-2">
@@ -17,9 +17,9 @@ class MyValues extends React.Component {
             <ul>
               <li className="my-values-item">Working in Central London</li>
               <li className="my-values-item">Having the time to balance work and private life</li>
-              <li className="my-values-item">SCRUM</li>
+              <li className="my-values-item">SCRUM/Kanban</li>
               <li className="my-values-item">Let's do-it approach</li>
-              <li className="my-values-item">Problem solving attitude</li>
+              <li className="my-values-item">Problem solving attitudes</li>
               <li className="my-values-item">Flat/horizontal hierarchies</li>
               <li className="my-values-item">Unit Testing</li>
               <li className="my-values-item">Apple Macs & Linux</li>
@@ -30,7 +30,7 @@ class MyValues extends React.Component {
             <h2>Sad face</h2>
 
             <ul>
-              <li className="my-values-item">Traveling</li>
+              <li className="my-values-item">Traveling (please don't!)</li>
               <li className="my-values-item">Working more than 9 hours/day at the office (but I can continue working from home, no probl. with that)</li>
               <li className="my-values-item">The practice of <a className="link" href="https://en.wikipedia.org/wiki/Pair_programming" target="_blank">"Agile Alliance XP pair-programming"</a> (this is not the same as 'working with a colleague".. I love that!)</li>
               <li className="my-values-item">The use of MS Windows</li>
