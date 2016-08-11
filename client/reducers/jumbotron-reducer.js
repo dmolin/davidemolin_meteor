209448ac055920abe04d401db73aa62a0df9b7d0
dmolin <davide.molin@gmail.com>
2016-08-11 23:54:13 +0100

    adding explanation on how lazy loading is implemented in the Jumbotron

--- client/reducers/jumbotron-reducer.js
+++ client/reducers/jumbotron-reducer.js
@@ -1,5 +1,11 @@
 import {JUMBOTRON_SLIDE} from '../actions/jumbotron-actions'
 
+/**
+ *  highestIndex is used to implement a basic lazy loading mechanism.
+ *  It represent the highest index of a visited slide (and, by extension, how many of them we already loaded)
+ *  When rendering out the Jumbotron, we'll load the images of the slide indexes <= highestIndex, while the others
+ *  will not be loaded yet, to save bandwidth and improve performances
+ **/
 export const initialState = {
   activeIndex: 0,
   highestIndex: 0
