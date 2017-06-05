f8e810d39e0d05f78904e7d88a3e4c7dd2756ac4
dmolin <davide.molin@gmail.com>
2017-06-05 15:14:38 +0200

    adding new logo

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -12,7 +12,7 @@ class Navbar extends React.Component {
         <div className="navbar container pure-g">
           <div className="navbar-brand pure-u-1 pure-u-md-1-4">
             <a href="/" className="navbar-brand-logo">
-              <h1 className="structural">Codingshack ltd</h1>
+              <h1 className="structural">Davide Molin</h1>
             </a>
           </div>
           <nav className="navbar-items navigation pure-u-1 pure-u-md-3-4">
